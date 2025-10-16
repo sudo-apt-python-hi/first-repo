cd4c2497b6d235bbb707e759d9e50cc467cd6d28
sudo-apt-python-hi <prajan.21012014@gmail.com>
2025-10-15 21:15:49 -0500

    added code files

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('worked :D')
+const space_facts_url = "https://en.wikipedia.org/wiki/Expansion_of_the_universe";
+function source(url) {
+    console.log('facts source:')
+    console.log(url)
+    alert('facts source: \n' + url)
+}
